b7435239d51e41ad8544a33cd7c2e6ef02c0824c
Cristhian Melo <rxtsel@hotmail.com>
2024-04-30 21:46:39 -0500

    chore(hypr): update hyprland config

--- hypr/gtk.sh
+++ hypr/gtk.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
 gnome_schema="org.gnome.desktop.interface"
-
-gsettings set "$gnome_schema" gtk-theme "Orchis-Dark"
-gsettings set "$gnome_schema" icon-theme "kora"
-gsettings set "$gnome_schema" cursor-theme "Bibata-Modern-Classic"
-gsettings set "$gnome_schema" font-name "Noto Sans 11"
-
+gsettings set "$gnome_schema" font-name "San Francisco Pro Display 10"
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
