4918aec02489c419c7fd332a5bde7fc42bbb25e9
Cristhian Melo <rxtsel@hotmail.com>
2024-05-15 09:43:13 -0500

    feat(utils): add update discord script

---
+++ utils/update_discord.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+remove_old_version() {
+	sudo rm -rf /opt/discord
+	sudo rm -rf /usr/bin/discord
+  sudo rm -rf /usr/share/discord
+	sudo rm -rf /usr/share/applications/discord.desktop
+}
+
+install_new_version() {
+	cd ~/Downloads/ &&
+		tar -xvzf discord-*.tar.gz &&
+		sudo mv Discord /opt/discord &&
+    sudo cp -rp /opt/discord/ /usr/share/ &&
+		sudo ln -sf /opt/discord/Discord /usr/bin/discord &&
+		sudo cp /opt/discord/discord.desktop /usr/share/applications/discord.desktop &&
+}
+
+echo "Removing old version of Discord..."
+remove_old_version
+echo "Installing new version of Discord..."
+install_new_version
+echo "Done! :)"
